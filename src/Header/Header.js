e0f7a681d7992a481876add4a4508b782a7a527d
virtualsports <tigran.darbinyan@betconstruct.com>
2021-02-16 20:08:17 +0400

    Implement Web site header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,9 +1,30 @@
 import './Header.scss'
 import React from "react"
+import {Link} from "react-router-dom"
+import About from "../About/About"
 
 function Header() {
+  const baseURL = process.env.PUBLIC_URL + './assets/header/';
+
   return <div className="header">
-    HEADER
+    <div className="header-container">
+      <div className="logo">
+        <img src={baseURL + 'logo.svg'} alt=''/>
+        <div className='arrow-down'/>
+      </div>
+      <div className='menu'>
+        <Link className='about-button' to={About}>About Us</Link>
+        <a href='https://www.instagram.com/dzook_app/' target='_blank' rel='noreferrer'>
+          <img src={baseURL + 'instagram.svg'} alt=''/>
+        </a>
+        <a href='https://www.fb.com/3dzookgifts' target='_blank' rel='noreferrer'>
+          <img src={baseURL + 'fb.svg'} alt=''/>
+        </a>
+        <a href='https://www.tiktok.com/@dzook' target='_blank' rel='noreferrer'>
+          <img src={baseURL + 'tiktok.svg'} alt=''/>
+        </a>
+      </div>
+    </div>
   </div>
 }
 
