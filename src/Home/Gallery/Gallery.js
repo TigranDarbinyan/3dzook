6bb68b778dc9ba398e3e6c947feab5a37d9f51af
virtualsports <tigran.darbinyan@betconstruct.com>
2021-03-24 16:30:20 +0400

    Update Gallery logic

--- src/Home/Gallery/Gallery.js
+++ src/Home/Gallery/Gallery.js
@@ -3,45 +3,20 @@ import './Gallery.scss'
 import Masonry, {ResponsiveMasonry} from "react-responsive-masonry"
 
 function Gallery() {
-  const [overData, setOverData] = useState(null);
+  const [selectedData, setSelectedData] = useState(null);
   const baseURL = process.env.PUBLIC_URL + '/assets/gallery/';
-  const images = [
-    baseURL + "01.jpg",
-    baseURL + "02.jpg",
-    baseURL + "03.jpg",
-    baseURL + "04.jpg",
-    baseURL + "05.jpg",
-    baseURL + "06.jpg",
-    baseURL + "07.jpg",
-    baseURL + "08.jpg",
-    baseURL + "09.jpg",
-    baseURL + "10.jpg",
-    baseURL + "11.jpg",
-    baseURL + "12.jpg",
-    baseURL + "13.jpg",
-    baseURL + "14.jpg",
-    baseURL + "15.jpg",
-    baseURL + "16.jpg",
-    baseURL + "17.jpg",
-    baseURL + "18.jpg",
-  ]
-
-  const videos = [
-    baseURL + "video_1.mp4",
-    /*baseURL + "video_2.mp4",
-    baseURL + "video_3.mp4",
-    baseURL + "video_4.mp4"*/
-  ]
+  const itemsCount = 54;
+  const videoIndexes = [11, 15, 19, 26, 35, 42, 49];
 
   const showResult = () => {
     return;
-    if (overData === null) {
+    if (selectedData === null) {
       return null;
     }
 
     return <div className='result-pop-up' style={{
-      top: `${overData[0]}px`,
-      left: `${overData[1]}px`,
+      top: `${selectedData[0]}px`,
+      left: `${selectedData[1]}px`,
     }}>
       <div className='close'>&times;</div>
       <div className='result-container'>
@@ -50,26 +25,35 @@ function Gallery() {
     </div>
   }
 
+  const getGalleryItems = () => {
+    const items = [];
+
+    for (let i = 1; i <= itemsCount; ++i) {
+      if (videoIndexes.indexOf(i) === -1) {
+        items.push(<img
+          key={i}
+          src={baseURL + (i).toString() + '.jpg'}
+          alt=""
+          style={{width: "100%", display: "block"}}
+          onMouseOver={e => e.target.src = baseURL + (i).toString() + '_result.jpg'}
+          onMouseOut={e => e.target.src = baseURL + (i).toString() + '.jpg'}
+        />)
+      } else {
+        items.push(
+          <video controls key={i} style={{width: "100%", display: "block"}}>
+            <source src={baseURL + (i).toString() + '.mov'} type="video/mp4"/>
+          </video>
+        )
+      }
+    }
+
+    return items;
+  }
+
   return <div className='gallery'>
     <ResponsiveMasonry columnsCountBreakPoints={{350: 1, 750: 2, 900: 3}}>
       <Masonry columnsCount={3} gutter="10px">
-        {images.map((image, i) => (
-          <img
-            key={i}
-            src={baseURL + (i + 1).toString() + '.jpg'}
-            alt=""
-            style={{width: "100%", display: "block"}}
-            onMouseOver={e => e.target.src = baseURL + (i + 1).toString() + '_result.jpg'}
-            onMouseOut={e => e.target.src = baseURL + (i + 1).toString() + '.jpg'}
-          />
-        ))}
-        {
-          videos.map((video, i) => (
-            <video controls key={i} style={{width: "100%", display: "block"}}>
-              <source src={video} type="video/mp4"/>
-            </video>
-          ))
-        }
+        {getGalleryItems()}
         {showResult()}
       </Masonry>
     </ResponsiveMasonry>
